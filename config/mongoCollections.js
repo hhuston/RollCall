8efaae3b3308a3344d0de888e436d9716f5e7d72
jesseknuckles <jknuckle@stevens.edu>
2024-11-25 06:28:51 -0500

    finished the first nine db functions, and got them pretty robust with a few exceptions

---
+++ config/mongoCollections.js
@@ -0,0 +1,20 @@
+import {dbConnection} from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+// NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT
+export const organizations = getCollectionFn('organizations');
+export const users = getCollectionFn('users');
+export const sessions = getCollectionFn('sessions');
+export const actions = getCollectionFn('actions');
---
+++ config/settings.js
@@ -0,0 +1,5 @@
+export const mongoConfig = {
+    //serverUrl: 'mongodb://localhost:27017/',
+    serverUrl: 'mongodb+srv://jknuckle:MongoKnuckShred101$@cluster0.efs3s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    database: 'RollCall'
+  };
