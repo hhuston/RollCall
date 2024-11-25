46945e42f0704e812d8cd51ab70337fe5bf8178e
JoelSimon1 <qazxcvbnmlpoiuytrewsdfghjkaaaa@gmail.com>
2024-11-24 19:17:36 -0500

    Added db info

---
+++ config/mongoCollections.js
@@ -0,0 +1,22 @@
+import {dbConnection} from './mongoConnection.js';
+
+/* This will allow you to have one reference to each collection per app */
+/* Feel free to copy and paste this this */
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+/* Now, you can list your collections here: */
+export const users = getCollectionFn('users');
+export const organizations = getCollectionFn('organizations');
+export const sessions = getCollectionFn('sessions');
+export const actions = getCollectionFn('actions');
