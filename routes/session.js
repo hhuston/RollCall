9e23fd99a62ef6613dd6dc1c42379db4d3f85917
hhuston <90809677+hhuston@users.noreply.github.com>
2024-12-14 17:06:10 -0500

    Added session handlebars and create initial setup

---
+++ routes/session.js
@@ -0,0 +1,9 @@
+import Router from "express";
+const router = Router();
+import { sessionData } from "../data/index.js";
+
+import validation from "../validation.js";
+
+router.use
+
+export default router;
