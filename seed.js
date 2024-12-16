4c251ac47f75f998b605253121f3986043c585b9
hhuston <90809677+hhuston@users.noreply.github.com>
2024-12-15 23:31:52 -0500

    Update Seed for new DB functions

--- seed.js
+++ seed.js
@@ -9,7 +9,7 @@ const sessionCollection = await sessions();
 const actionCollection = await actions();
 
 // Clear DB
-console.log("Clear MongoDB Collections...");
+console.log("Clearing MongoDB Collections...");
 await userCollection.deleteMany({});
 await orgCollection.deleteMany({});
 await sessionCollection.deleteMany({});
@@ -29,51 +29,57 @@ for (let i in testUsers) {
     testUsers[i] = await userData.createUser(user.userName, user.password, user.firstName, user.lastname, user.email);
 }
 
-console.log("Inserting test actions...");
-const testActions = [
-    {_id: '', type: 'Motion', value: 'Motion to close session', actionOwner: testUsers[0].userName},
-    {_id: '', type: 'Motion', value: 'Motion to open moderated debate', actionOwner: testUsers[1].userName},
-    {_id: '', type: 'Proposal', value: 'Sessions should be held twice a week', actionOwner: testUsers[1].userName},
-    {_id: '', type: 'Proposal', value: 'Sessions should be held thrice a week', actionOwner: testUsers[2].userName},
-    {_id: '', type: 'Motion', value: 'Motion to censor Steve', actionOwner: testUsers[0].userName},
-    {_id: '', type: 'Motion', value: 'Motion to close session', actionOwner: testUsers[0].userName},
-    {_id: '', type: 'Motion', value: 'Motion to open moderated debate', actionOwner: testUsers[1].userName},
-    {_id: '', type: 'Proposal', value: 'Sessions should be held twice a week', actionOwner: testUsers[1].userName},
-    {_id: '', type: 'Proposal', value: 'Sessions should be held thrice a week', actionOwner: testUsers[2].userName},
-    {_id: '', type: 'Motion', value: 'Motion to censor Steve', actionOwner: testUsers[0].userName},
-];
-for (let i in testActions) {
-    let action = testActions[i];
-    testActions[i]._id = await actionData.createAction(action.type, action.value, action.actionOwner);
-}
-
-console.log("Inserting test sessions...")
-const testSessions = [
-    {_id: '', proposal: 'Session should be held once a week', proposalOwner: testUsers[0].userName, sessionDate: new Date().toUTCString()},
-    {_id: '', proposal: 'Harrison should be given $1000', proposalOwner: testUsers[4].userName, sessionDate: new Date().toUTCString()},
-];
-for (let i in testSessions) {
-    let session = testSessions[i];
-    testSessions[i]._id = await sessionData.createSession(session.proposal, session.proposalOwner, session.sessionDate);
-}
-
 console.log("Inserting test organizations...");
 const testOrgs = [
-    {_id: '', orgName: 'First Presbyterian Church of Ramsey', password: 'pass', userName: testUsers[4].userName},
+    {_id: '', orgName: 'First Presbyterian Church of Ramsey', password: 'Password@1', userName: testUsers[4].userName},
 ];
 
 for (let i in testOrgs) {
     let org = testOrgs[i];
     testOrgs[i]._id = (await organizationData.createOrganization(org.orgName, org.password, org.userName))._id;
 }
-console.log(testOrgs);
 
-console.log("Adding user to organization...");
+console.log("Adding users to organization...");
 for (let i in testUsers) {
     if (i == 4) break;
     const returnData = await organizationData.loginOrg(testUsers[i].userName, testOrgs[0].password, testOrgs[0].orgName, "Member");
 
     testOrgs[0].members = returnData[1].members;
+}
+
+console.log("Inserting test sessions...")
+const testSessions = [
+    {_id: '', proposal: 'Session should be held once a week', proposalOwner: testUsers[0].userName, orgName: 'First Presbyterian Church of Ramsey', seshName: "How many sessions?"},
+    {_id: '', proposal: 'Harrison should be given $1000', proposalOwner: testUsers[4].userName, orgName: 'First Presbyterian Church of Ramsey', seshName: "Give Harrison money?"},
+];
+for (let i in testSessions) {
+    let session = testSessions[i];
+    testSessions[i] = await sessionData.createSession(session.proposal, session.proposalOwner, session.orgName, session.seshName);
+}
+
+console.log("Adding users to test sessions");
+for (let i in testUsers) {
+    if (i == 0) continue;
+    testSessions[0] = await sessionData.joinSession(testSessions[0]._id, "Voter",testUsers[i].userName);
+}
+
+console.log("Inserting test actions...");
+const testActions = [
+    {_id: '', type: 'Motion', value: 'Motion to close session', actionOwner: testUsers[0].userName, sessionId: testSessions[0]._id},
+    {_id: '', type: 'Motion', value: 'Motion to open moderated debate', actionOwner: testUsers[1].userName, sessionId: testSessions[0]._id},
+    {_id: '', type: 'Amendment', value: 'Sessions should be held twice a week', actionOwner: testUsers[1].userName, sessionId: testSessions[0]._id},
+    {_id: '', type: 'Amendment', value: 'Sessions should be held thrice a week', actionOwner: testUsers[2].userName, sessionId: testSessions[0]._id},
+    {_id: '', type: 'Motion', value: 'Motion to censor Steve', actionOwner: testUsers[0].userName, sessionId: testSessions[0]._id},
+    {_id: '', type: 'Motion', value: 'Motion to close session', actionOwner: testUsers[0].userName, sessionId: testSessions[0]._id},
+    {_id: '', type: 'Motion', value: 'Motion to open moderated debate', actionOwner: testUsers[1].userName, sessionId: testSessions[0]._id},
+    {_id: '', type: 'Amendment', value: 'Sessions should be held twice a week', actionOwner: testUsers[1].userName, sessionId: testSessions[0]._id},
+    {_id: '', type: 'Amendment', value: 'Sessions should be held thrice a week', actionOwner: testUsers[2].userName, sessionId: testSessions[0]._id},
+    {_id: '', type: 'Motion', value: 'Motion to censor Steve', actionOwner: testUsers[0].userName, sessionId: testSessions[0]._id},
+];
+for (let i in testActions) {
+    let action = testActions[i];
+    testActions[i]._id = await actionData.createAction(action.type, action.value, action.actionOwner, action.sessionId);
+    testSessions[0] = await sessionData.getSession(action.sessionId);
 }
 
 console.log("Done");
