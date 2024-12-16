7aa868928516eae84afbf851ccc4ae2a4d07bfee
JoelSimon3 <jsimon7@stevens.edu>
2024-12-15 23:53:39 -0500

    Added ability to leave a session

--- data/sessions.js
+++ data/sessions.js
@@ -1,5 +1,4 @@
 import { sessions, users, organizations } from "../config/mongoCollections.js";
-import { ObjectId } from "mongodb";
 import validation from "../validation.js";
 
 let createSession = async (proposal, proposalOwner, orgName, seshName) => {
@@ -100,4 +99,16 @@ let deleteSession = async (id) => {
     return { ...deletionInfo, deleted: true };
 };
 
-export default { createSession, deleteSession, getSession, joinSession };
+let leaveSession = async (sessionId, userName) => {
+    sessionId = validation.checkId(sessionId);
+    userName = validation.checkUserName(userName);
+
+    const sessionCollection = await sessions();
+    const updatedInfo = await sessionCollection.findOneAndUpdate({ _id: sessionId }, { $pull: { members: userName } }, { projection: { _id: 0, orgName: 1 } });
+
+    if (!updatedInfo) throw "Could not remove user from session";
+
+    return updatedInfo.orgName;
+};
+
+export default { createSession, deleteSession, getSession, joinSession, leaveSession };
